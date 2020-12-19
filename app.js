2b11781106dc81298ece708913b32447ff02b144
Meme Man <tomekslawny@gmail.com>
2020-12-19 17:58:26 +0100

    types

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Your code...');
+function add(n1, n2, showResult, phrase) {
+    if (showResult) {
+        console.log(phrase + n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var resultPhrase = 'Result is: ';
+add(number1, number2, printResult, resultPhrase);
