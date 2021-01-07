f9e416eb8f0700e1df37405f4331d55b175e914a
Meme Man <tomekslawny@gmail.com>
2021-01-07 20:50:27 +0100

    aliases and void commit

--- app.ts
+++ app.ts
@@ -1,21 +1,9 @@
-function combine(input1: number | string, input2: number | string, resultConversion: string) {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-    if (resultConversion === 'as-number'){
-        return +result;
-    } else {
-        return result.toString();
-    }}
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
 
-    const combinedAges = combine(30, 26, 'as-number');
-    console.log(combinedAges)
+function printResult(num: number): void {
+    console.log('Result: ' + num);
+}
 
-    const combinedStringAges = combine('30', '26', 'as-number');
-    console.log(combinedStringAges)
-
-    const combinedNames = combine('Max', 'Anna', 'as-text');
-    console.log(combinedNames)
+printResult(add(5,12));
