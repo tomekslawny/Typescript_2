d87f16d6784c2658cfe42406c05f4f2cbccc1f4d
Meme Man <tomekslawny@gmail.com>
2021-01-09 19:42:31 +0100

    final commit

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput =5;
+userInput ='Max';
+if(typeof userInput === "string") {
+    userName = userInput;
 }
 
-function printResult(num: number): void {
-    console.log('Result: ' + num);
+function generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code};
+    // white (true) {}
 }
 
-printResult(add(5,12));
+generateError('An error occured', 500)
